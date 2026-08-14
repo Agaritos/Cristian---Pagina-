71513d0406d12972e76386db029c57430df17f22
mrojascusoftware <marcos.rojas@pi.edu.co>
2026-08-13 20:34:12 -0500

    cree la base de datos y la conexion

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mysql = require ("mysql2")
+const pool = mysql.createPool({
+
+host : 'localhost',
+user : 'root',
+password : '',
+port : 3306,
+database :administrador_dinero22
+
+
+})
+module.exports =pool.promise()
